f64e9ad73eda82fe68ed2203fa64e56a2de590f7
svvladimir-ru <svetlakov454@gmail.com>
2021-09-13 17:30:29 +0300

    добавил cp в sh script

--- up.sh
+++ up.sh
@@ -3,6 +3,8 @@ echo "Запуск postgres"
 docker-compose up --build -d ma_postgres
 sleep 5
 echo "Начало загрузки данных в postgres"
+cp .env.sample sqlite_to_postgres/.env
+cp .env.sample movies_admin/.env
 cd sqlite_to_postgres/ || exit
 ../venv/bin/python load_data.py
 cd ..
