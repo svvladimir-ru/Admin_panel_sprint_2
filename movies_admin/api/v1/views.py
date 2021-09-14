00013a143d05a1aa496fe5820f7d2a01830b6655
svvladimir-ru <svetlakov454@gmail.com>
2021-09-14 09:58:53 +0300

    Правки по 2 ревью(compose, views, nginx)

--- movies_admin/api/v1/views.py
+++ movies_admin/api/v1/views.py
@@ -4,30 +4,29 @@ from django.views.generic.detail import BaseDetailView
 from django.db.models import Q
 from django.contrib.postgres.aggregates import ArrayAgg
 
-from movies.models import FilmWork
+from movies.models import FilmWork, PersonRole
 
 
 class MoviesApiMixin:
     model = FilmWork
     http_method_names = ['get']
 
-    @staticmethod
-    def get_queryset():
-        qs = FilmWork.objects.values('id', 'title', 'description', 'creation_date', 'rating', 'type'
+    def _aggregate_person(role: str):
+        return ArrayAgg(
+            'personfilmwork__person__full_name',
+            filter=Q(personfilmwork__role=role),
+            distinct=True,
+        )
+
+    @classmethod
+    def get_queryset(cls):
+        return FilmWork.objects.values('id', 'title', 'description', 'creation_date', 'rating', 'type'
                 ).annotate(
-                    genres=ArrayAgg('genres__name', distinct=True)
-                ).annotate(
-                    actors=ArrayAgg('personfilmwork__person__full_name',
-                    filter=Q(personfilmwork__role='actor'), distinct=True)
-                ).annotate(
-                    directors=ArrayAgg('personfilmwork__person__full_name',
-                    filter=Q(personfilmwork__role='director'), distinct=True)
-                ).annotate(
-                    writers=ArrayAgg('personfilmwork__person__full_name',
-                    filter=Q(personfilmwork__role='writer'), distinct=True)
-        )  # Не совсем понял с PersonRole, но попробую подумать какие еще есть варианты. Я просто изначально пробовал
-        # без annotate, но не получилось сделать красиво и правильно)
-        return qs
+                    genres=ArrayAgg('genres__name', distinct=True),
+                    actors=cls._aggregate_person(role=PersonRole.ACTOR),
+                    directors=cls._aggregate_person(role=PersonRole.DIRECTOR),
+                    writers=cls._aggregate_person(role=PersonRole.WRITER)
+        )
 
     @staticmethod
     def render_to_response(context, **response_kwargs):
@@ -39,7 +38,7 @@ class Movies(MoviesApiMixin, BaseListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         paginator, page, queryset, is_paginated = self.paginate_queryset(
-            list(self.get_queryset()),
+            self.get_queryset(),
             self.paginate_by
         )
         context = {
@@ -47,7 +46,7 @@ class Movies(MoviesApiMixin, BaseListView):
             'total_pages': paginator.num_pages,
             'prev': page.previous_page_number() if page.has_previous() else None,
             'next': page.next_page_number() if page.has_next() else None,
-            'results': queryset,
+            'results': list(queryset),
         }
         return context
 
